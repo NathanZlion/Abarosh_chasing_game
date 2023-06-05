841dfa9aa1a3c547f5f181ca3efdf50a75d2c47f
Nathnael Dereje <nathandere1357@gmail.com>
2023-06-05 04:17:09 +0300

    made the tested new code part of Game.py

--- Game.py
+++ Game.py
@@ -14,9 +14,14 @@ class Game:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
         self.is_running = True
+        self.splash_screen = True
+        self.chaser_score = 0
+        self.chased_score = 0
+        self.splash_animation_frames = []
+        self.load_splash_animation_frames()  # Load animation frames
+
         self.CHASER = Chaser(
             game=self,
-            position=CHASER_INITIAL_POSITION,
             color=RED,
             speed=CHASER_SPEED,
         )
@@ -83,6 +88,20 @@ class Game:
         if keys_pressed[K_RIGHT]:
             self.CHASED.rotate("right")
 
+    def check_collisions(self):
+        chaser_rect = self.CHASER.get_rect()
+        chased_rect = self.CHASED.get_rect()
+
+        if chaser_rect.colliderect(chased_rect):
+            self.chaser_score += 1
+            self.CHASER.reset_position()
+            self.CHASED.reset_position()
+
+        goal_safe_zone = self.safe_zones[-1]
+        if chased_rect.colliderect(goal_safe_zone):
+            self.chased_score += 1
+            self.CHASED.reset_position()
+
     def draw(self):
         self.screen.fill((0, 0, 0))  # Clear screen
         for safe_zone in self.safe_zones:
@@ -93,12 +112,73 @@ class Game:
             # draw the obstacles so that they look like trees
             pygame.draw.rect(self.screen, BROWN, obstacle, 0, 20, 20, 20, 20)
 
+        # Display scores
+        font = pygame.font.SysFont("None", 36)
+        chaser_score_text = font.render(
+            f"Chaser Score: {self.chaser_score}", True, WHITE
+        )
+        chased_score_text = font.render(
+            f"Chased Score: {self.chased_score}", True, WHITE
+        )
+        self.screen.blit(chaser_score_text, (10, 10))
+        self.screen.blit(chased_score_text, (10, 50))
+
         pygame.display.flip()
 
+    def load_splash_animation_frames(self):
+        for i in range(0, 14):
+            if i < 10:
+                i = f"0{i}"
+
+            frame = pygame.transform.scale(
+                pygame.image.load(f"./images/splash{i}.png").convert_alpha(), (800, 600)
+            )
+            self.splash_animation_frames.append(frame)
+
+    def show_splash_screen(self):
+        frame_index = 0
+        animation_delay = 100  # Delay between animation frames (in milliseconds)
+        animation_timer = 0
+
+        while self.splash_screen:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    quit()
+
+            self.screen.fill((0, 0, 0))  # Clear screen
+
+            # Display animation frame
+            self.screen.blit(self.splash_animation_frames[frame_index], (0, 0))
+
+            pygame.display.flip()
+
+            # Update animation frame
+            animation_timer += self.clock.get_time()
+            if animation_timer >= animation_delay:
+                frame_index += 1
+                animation_timer = 0
+
+            # Break out of splash screen after all frames have been displayed
+            if frame_index >= len(self.splash_animation_frames):
+                break
+
+            self.clock.tick(60)  # Limit frame rate
+
+        pygame.time.wait(2000)  # Wait for 2 seconds after animation
+        self.splash_screen = False
+
     def run(self):
+        self.show_splash_screen()
+
         while self.is_running:
             self.handle_events()
-            self.draw()
-            self.clock.tick(10)  # Limit frame rate
+
+            if not self.splash_screen:
+                self.check_collisions()
+                self.draw()
+
+            # Limit frame rate
+            self.clock.tick(10)
 
         pygame.quit()
